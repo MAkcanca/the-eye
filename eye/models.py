e5a3a22cca98e137ae4ddfe1e751112abf8231dd
Mustafa Akcanca <mustafa.the.science.guy@gmail.com>
2022-01-07 22:32:03 +0300

    Add Event model
    Modify Event model for description

--- eye/models.py
+++ eye/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    session_id = models.CharField(max_length=150)
+    timestamp = models.DateTimeField()  # Susceptible to modify
+    category = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+    data = models.JSONField()
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.session_id} - {self.category} - {self.name} - {self.timestamp}"
